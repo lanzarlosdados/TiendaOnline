373ec5ae1d0f572e43dc36ef9dca375dbfcde45f
fabian <zaratefabian@hotmail.com>
2022-01-12 20:05:48 -0300

    funcionalidad VER MAS productos ,utilizando archivo JSON

--- js/datos.js
+++ js/datos.js
@@ -1,7 +1,40 @@
-function traerDatos(){
-    fetch('json\productos.json')
-    .then(res => res.json)
+const btnProductos = document.getElementById('agregarProductos');
+
+btnProductos.addEventListener('click', (event) => {
+    event.preventDefault();
+    fetch('json/productos.json')
+    .then(res => res.json())
     .then(datos => {
-        console.log(datos)
-    })
+        mostrarDatos(datos);
+    }) 
 }
+    );
+
+    function mostrarDatos(datos){
+ 
+    for (let valor of datos) {
+    
+        sectionLentes.innerHTML +=  `
+              <div class="lentes">
+                   <img src="${valor.rutaImg}" alt="" class="img_lentes">
+                  <div class="content_text">
+                    <p class="lentes_text">${valor.nombre}</p>
+                    <p class="lentes_precio">${valor.precio}</p>
+                  </div>
+                  <div class="content_star">
+                       <div class="star icon-star"></div>
+                       <div class="star icon-star"></div>
+                       <div class="star icon-star"></div>
+                       <div class="star icon-star"></div>
+                       <div class="star icon-star"></div>
+                  </div>
+                  <div class="cart_plus icon-cart-plus"></div>
+               </div>
+    `;
+
+    }
+    
+    
+
+    }
+
